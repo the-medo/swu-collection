6597c22d4d0e8863155a78c505e11f6152626df5
Medo <martinmederly@gmail.com>
2025-01-05 23:46:07 +0100

    chore: setup shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,12 @@
+import { Button } from '@/components/ui/button.tsx';
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button>Asd</Button>
+    </div>
+  );
 }
 
 export default App;
