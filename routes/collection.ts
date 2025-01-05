cd18bee95271ff885e077315b1fad398671a9368
Medo <martinmederly@gmail.com>
2025-01-05 18:41:36 +0100

    feat: add zod and @hono/zod-validator, use them in post endpoint

--- routes/collection.ts
+++ routes/collection.ts
@@ -1,11 +1,23 @@
 import { Hono } from 'hono';
-import { fakeCollectionCards } from '../types/CollectionCard.ts';
+import { collectionCardSchema, fakeCollectionCards } from '../types/CollectionCard.ts';
+import { z } from 'zod';
+import { SwuSet } from '../types/SwuSet.ts';
+import { zValidator } from '@hono/zod-validator';
+
+const swuSetSchema = z.nativeEnum(SwuSet);
 
 export const collectionRoute = new Hono()
   .get('/', c => {
     return c.json({ collection: fakeCollectionCards });
   })
-  .post('/', async c => {
-    const collectionCard = await c.req.json();
+  .post('/', zValidator('json', collectionCardSchema), async c => {
+    const data = await c.req.valid('json');
+    const collectionCard = collectionCardSchema.parse(data);
+    c.status(201);
     return c.json(collectionCard);
+  })
+  .get('/:set', c => {
+    const set = swuSetSchema.parse(c.req.param('set'));
+    const collection = fakeCollectionCards.filter(fcc => fcc.set === set);
+    return c.json({ collection: collection });
   });
--- types/CollectionCard.ts
+++ types/CollectionCard.ts
@@ -1,22 +1,29 @@
 import { SwuSet } from './SwuSet.ts';
+import { z } from 'zod';
 
-export type CollectionCard = {
-  set: SwuSet;
-  setNumber: number;
-  owned: number;
-  foil?: boolean;
-  hyperspace?: boolean;
-};
+const userCollectionCardSchema = z.object({
+  userId: z.number().int().positive(),
+  set: z.nativeEnum(SwuSet),
+  setNumber: z.number().int().positive().min(1),
+  owned: z.number().int().default(0),
+  wanted: z.number().int().default(0),
+  foil: z.boolean().optional().default(false),
+  hyperspace: z.boolean().optional().default(false),
+});
+
+export const collectionCardSchema = userCollectionCardSchema.omit({ userId: true });
+
+export type CollectionCard = z.infer<typeof collectionCardSchema>;
 
 export const fakeCollectionCards: CollectionCard[] = [
-  { set: SwuSet.SHD, setNumber: 111, owned: 2, foil: true },
-  { set: SwuSet.SOR, setNumber: 222, owned: 5, foil: true, hyperspace: true },
-  { set: SwuSet.TWI, setNumber: 133, owned: 1 },
-  { set: SwuSet.SHD, setNumber: 144, owned: 3, hyperspace: true },
-  { set: SwuSet.SOR, setNumber: 55, owned: 6, foil: true },
-  { set: SwuSet.TWI, setNumber: 66, owned: 3, foil: true, hyperspace: true },
-  { set: SwuSet.SHD, setNumber: 77, owned: 4 },
-  { set: SwuSet.SOR, setNumber: 88, owned: 7, hyperspace: true },
-  { set: SwuSet.TWI, setNumber: 99, owned: 2, foil: true },
-  { set: SwuSet.SHD, setNumber: 100, owned: 1, foil: true, hyperspace: true },
+  { set: SwuSet.SHD, setNumber: 111, owned: 2, wanted: 0, foil: true, hyperspace: false },
+  { set: SwuSet.SOR, setNumber: 222, owned: 5, wanted: 0, foil: true, hyperspace: true },
+  { set: SwuSet.TWI, setNumber: 133, owned: 1, wanted: 0, foil: false, hyperspace: false },
+  { set: SwuSet.SHD, setNumber: 144, owned: 3, wanted: 0, foil: false, hyperspace: true },
+  { set: SwuSet.SOR, setNumber: 55, owned: 6, wanted: 0, foil: true, hyperspace: false },
+  { set: SwuSet.TWI, setNumber: 66, owned: 3, wanted: 0, foil: true, hyperspace: true },
+  { set: SwuSet.SHD, setNumber: 77, owned: 4, wanted: 0, foil: true, hyperspace: false },
+  { set: SwuSet.SOR, setNumber: 88, owned: 7, wanted: 0, foil: false, hyperspace: true },
+  { set: SwuSet.TWI, setNumber: 99, owned: 2, wanted: 0, foil: true, hyperspace: false },
+  { set: SwuSet.SHD, setNumber: 100, owned: 1, wanted: 0, foil: true, hyperspace: true },
 ];
