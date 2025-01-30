75338b91e9e6a9f0023042d1dcaf99c172aa5783
Medo <martinmederly@gmail.com>
2025-01-30 20:38:18 +0100

    style: add global typography styles for h and p tags

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
+import './styles/index.css';
+import './styles/typography.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createRouter, RouterProvider } from '@tanstack/react-router';
 import { routeTree } from './routeTree.gen';
