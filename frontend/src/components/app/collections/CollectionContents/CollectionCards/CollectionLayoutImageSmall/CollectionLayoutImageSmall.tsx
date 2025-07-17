e97ffbe22beed8bf88d549892baca157ddb610de
Medo <martinmederly@gmail.com>
2025-07-17 19:07:28 +0200

    feat: rework image collection layouts - extract to subcomponents

--- frontend/src/components/app/collections/CollectionContents/CollectionCards/CollectionLayoutImageSmall/CollectionLayoutImageSmall.tsx
+++ frontend/src/components/app/collections/CollectionContents/CollectionCards/CollectionLayoutImageSmall/CollectionLayoutImageSmall.tsx
@@ -1,13 +1,9 @@
 import { useCardList } from '@/api/lists/useCardList.ts';
-import CardImage, { cardImageVariants } from '@/components/app/global/CardImage.tsx';
+import { cardImageVariants } from '@/components/app/global/CardImage.tsx';
 import { Skeleton } from '@/components/ui/skeleton.tsx';
 import { cn } from '@/lib/utils.ts';
-import CollectionCardHoverDetail from '@/components/app/collections/CollectionCardDetail/CollectionCardHoverDetail.tsx';
 import { useCollectionInfo } from '@/components/app/collections/CollectionContents/CollectionSettings/useCollectionLayoutStore.ts';
-import {
-  useCCDetail,
-  useCCCard,
-} from '@/components/app/collections/CollectionContents/CollectionGroups/useCollectionGroupStore.ts';
+import SmallCardItem from './SmallCardItem';
 
 interface CollectionLayoutImageSmallProps {
   collectionId: string;
@@ -45,39 +41,14 @@ const CollectionLayoutImageSmall: React.FC<CollectionLayoutImageSmallProps> = ({
           );
         }
 
-        const collectionCard = useCCDetail(cardKey);
-        const card = useCCCard(cardKey);
-
         return (
-          <CollectionCardHoverDetail
+          <SmallCardItem
             key={cardKey}
-            cardData={card}
+            cardKey={cardKey}
             collectionId={collectionId}
-            collectionCard={collectionCard}
-          >
-            <div
-              className="max-w-[100px] flex flex-col gap-1 bg-secondary gray-200 rounded-lg"
-              key={`${collectionCard.variantId}-${collectionCard.foil}`}
-            >
-              <CardImage
-                card={card}
-                cardVariantId={collectionCard.variantId}
-                size="w100"
-                foil={collectionCard.foil}
-                forceHorizontal={horizontal}
-                backSideButton={false}
-              >
-                <div className="absolute bottom-0 right-0 w-fit flex flex-col grow-0 items-end mr-1 mb-1 bg-background opacity-90 rounded-lg">
-                  <div className="font-medium text-sm px-2">{collectionCard.amount}x</div>
-                </div>
-              </CardImage>
-              {collectionCard.price && (
-                <div className="text-xs p-1 pt-0 text-secondary-foreground text-right">
-                  {collectionCard.price} {currency}
-                </div>
-              )}
-            </div>
-          </CollectionCardHoverDetail>
+            horizontal={horizontal}
+            currency={currency}
+          />
         );
       })}
     </div>
