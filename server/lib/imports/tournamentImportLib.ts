80b2bc6b8ca274dcf0d27a1fb8466a8b7a56a163
Medo <martinmederly@gmail.com>
2025-09-07 01:55:08 +0200

    chore: tournament import

--- server/lib/imports/tournamentImportLib.ts
+++ server/lib/imports/tournamentImportLib.ts
@@ -17,16 +17,21 @@ export type TIUserDecklistMap = Record<number, TIBasicDecklistInfo | undefined>;
 
 const basicHeaders = {
   cookie: process.env.TOURNAMENT_COOKIE ?? '',
-  'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+  'sec-ch-ua': '"Not;A=Brand";v="99", "Google Chrome";v="139", "Chromium";v="139"',
   'sec-ch-ua-mobile': '?0',
   'sec-ch-ua-platform': '"Windows"',
-  'sec-fetch-dest': 'empty',
-  'sec-fetch-mode': 'cors',
-  'sec-fetch-site': 'same-origin',
+  'sec-fetch-dest': 'document',
+  'sec-fetch-mode': 'navigate',
+  'sec-fetch-site': 'cross-site',
+  'sec-fetch-user': '?1',
   'user-agent':
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36',
   'x-requested-with': 'XMLHttpRequest',
   origin: process.env.TOURNAMENT_ORIGIN ?? '',
+  accept:
+    'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+  'accept-encoding': 'gzip, deflate, br, zstd',
+  'accept-language': 'en,sk;q=0.9,cs;q=0.8,pt;q=0.7',
 };
 
 const formPostHeaders = {
@@ -53,7 +58,9 @@ export async function fetchTournamentView(
     });
 
     if (!response.ok) {
-      throw new Error(`Failed to fetch tournament view: ${response.status} ${response.statusText}`);
+      throw new Error(
+        `Failed to fetch tournament view: ${response.status} ${response.statusText} ${response}`,
+      );
     }
 
     const html = await response.text();
