ed2471ee5f8dbc70b219966072a835c1f8241b9a
Medo <martinmederly@gmail.com>
2025-10-29 22:53:48 +0100

    chore: another FE package version increases

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,13 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
 import svgr from 'vite-plugin-svgr';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svgr(), TanStackRouterVite(), react(), tailwindcss()],
+  plugins: [svgr(), tanstackRouter(), react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
