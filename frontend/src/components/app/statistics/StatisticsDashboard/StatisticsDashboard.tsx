0eb15850a8b99f2244557c0f44995a77f98d6ef5
Medo <martinmederly@gmail.com>
2026-01-30 01:08:55 +0100

    feat: recently played decks

--- frontend/src/components/app/statistics/StatisticsDashboard/StatisticsDashboard.tsx
+++ frontend/src/components/app/statistics/StatisticsDashboard/StatisticsDashboard.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { useGameResults } from '@/components/app/statistics/useGameResults.ts';
 import { Card } from '@/components/ui/card.tsx';
 import DashboardCalendar from '@/components/app/statistics/StatisticsDashboard/DashboardCalendar/DashboardCalendar.tsx';
+import DashboardDecks from '@/components/app/statistics/StatisticsDashboard/DashboardDecks/DashboardDecks.tsx';
 
 interface StatisticsDashboardProps {
   teamId?: string;
@@ -13,7 +14,14 @@ const StatisticsDashboard: React.FC<StatisticsDashboardProps> = ({ teamId }) => 
   return (
     <div className="flex flex-col gap-4">
       <Card className="p-4">
-        <DashboardCalendar matchesByDate={gameResultData?.matches.byDate} />
+        <div className="flex gap-4 flex-1">
+          <DashboardCalendar matchesByDate={gameResultData?.matches.byDate} />
+          {gameResultData && (
+            <div className="flex flex-1 gap-4">
+              <DashboardDecks byDeckId={gameResultData.matches.byDeckId} />
+            </div>
+          )}
+        </div>
       </Card>
     </div>
   );
