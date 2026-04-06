da8f8d1dd72d8bfd095a618682b4c9f63f7b5716
Medo <martinmederly@gmail.com>
2026-04-06 23:25:42 +0200

    fix: adjust min-max formatId parameter, version 5.3.6

--- frontend/src/routes/meta/index.tsx
+++ frontend/src/routes/meta/index.tsx
@@ -7,13 +7,7 @@ import MetaPage from '@/components/app/meta/MetaPage/MetaPage.tsx';
 export const DEFAULT_MIN_TOURNAMENT_TYPE = 'pq';
 
 const searchParams = z.object({
-  formatId: z
-    .number()
-    .int()
-    .min(formatData[0].id)
-    .max(formatData[formatData.length - 1].id)
-    .optional()
-    .default(formatData[0].id),
+  formatId: z.number().int().min(1).max(10).optional().default(formatData[0].id),
   metaId: z.number().int().min(1).optional(),
   minTournamentType: z
     .enum([...tournamentTypes])
